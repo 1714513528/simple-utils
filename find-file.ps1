30f4c6e029d15d711b166bb8ccb28d851fa0290b
1714513528 <50414528+1714513528@users.noreply.github.com>
2023-05-17 19:36:34 +0800

    Update find-file.ps1
    对代码进行优化，增加多个文件被搜索到的处理

--- find-file.ps1
+++ find-file.ps1
@@ -1,8 +1,12 @@
 FUNCTION Find-File([String] $pattern){
   $input=$((ls).Name|Select-String -AllMatches -SimpleMatch -Pattern $pattern);
-  if($null -ne $input){
-    explorer.exe /select,$input;
-  }else{
-    echo "FILE NOT FOUND !!!";
+  if($null -eq $input){
+    echo "$pattern NOT FOUND !!!";
+  }elseif($input.Length -eq 1){
+    explorer.exe /select,$input[0];
+  }elseif($input.Length -gt 1){
+    for($i=0;$i -lt $input.Length;$i++){$_=$input[$i];echo "$i`t$_";}
+    $index=Read-Host "please input index";
+    explorer.exe /select,$input[$index];
   }
 }
